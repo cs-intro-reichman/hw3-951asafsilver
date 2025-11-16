f9b32c635b02dd8af5f24a3a73d1eab5392e0ca8
Asaf Silver <asaf.silver@post.runi.ac.il>
2025-11-16 15:10:56 +0200

    completed

--- LoanCalc.java
+++ LoanCalc.java
@@ -1,5 +1,6 @@
 // Computes the periodical payment necessary to pay a given loan.
-public class LoanCalc {
+public class LoanCalc
+ {
 	
 	static double epsilon = 0.001;  // Approximation accuracy
 	static int iterationCounter;    // Number of iterations 
@@ -27,9 +28,13 @@ public class LoanCalc {
 
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
-	private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-		return 0;
+	private static double endBalance(double loan, double rate, int n, double payment) 
+	{	
+		for (int i=0; i<n; i++)// number of years
+		{
+			loan = ((loan - payment) * ((rate/100)+1));// calculates the loan*payment*rate 
+		}
+		return loan;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -38,8 +43,14 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
-		return 0;
+		iterationCounter = 0; 
+		double payment =loan/n;// מתחיל מתשלום התחלתי ללא ריבית, בטוח יהיה נמוך מדי
+		while (endBalance(loan,rate, n, payment)>0) // עד שהיתרה קטנה מאפס
+		{
+			iterationCounter++; // סופר איטרציות
+			payment = payment + epsilon;//כל פעם מעלה את התשלום באפסילון
+		}
+		return payment;
     }
     
     // Uses bisection search to compute an approximation of the periodical payment 
@@ -47,8 +58,29 @@ public class LoanCalc {
 	// Given: the sum of the loan, the periodical interest rate (as a percentage),
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
-    public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
+   public static double bisectionSolver(double loan, double rate, int n, double epsilon)
+    {
+    iterationCounter = 0; 
+    double minpayment = loan / n; //ערך מינימאלי
+    double maxpayment = minpayment; 
+    while (endBalance(loan, rate, n, maxpayment) > 0)// מוצא ערך מקסימלי
+	{
+        maxpayment = maxpayment * 2; 
     }
+    double midpayment = (minpayment + maxpayment) / 2;
+    while ((maxpayment - minpayment) > epsilon) // כל עוד הטווח גדול מאפסילון
+	{ 
+        iterationCounter++;
+        midpayment = (minpayment + maxpayment) / 2;
+        if (endBalance(loan, rate, n, midpayment) > 0) //אם נשארה יתרה
+		{
+            minpayment = midpayment; //מצמצם את הטווח מלמטה
+        } 
+        else //אם היתרה שלילית
+		{
+            maxpayment = midpayment; // מצמצם את הטווח מלמעלה
+        }
+    }
+    
+    return midpayment;
 }
